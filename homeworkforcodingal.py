9343f28ad2be83fd22b711287b49491117ee531a
CrazyCodingCassowary <suttisom.nattika@gmail.com>
2026-01-18 00:18:58 -0800

    Add files via upload

---
+++ homeworkforcodingal.py
@@ -0,0 +1,12 @@
+# Given tuple
+numbers = (2, 4, 6, 3)
+
+# Initialize product
+product = 1
+
+# Multiply each element
+for num in numbers:
+    product *= num
+
+# Print result
+print("The product of all numbers in the tuple is:", product)
